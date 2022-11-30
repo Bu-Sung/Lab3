a779aba0aa7be5dee01636ee0d388831ff50af33
Bu-Sung <qntjd4680@gmail.com>
2022-11-30 18:02:51 +0900

    Question6 complete

--- Question6/system_input.c
+++ Question6/system_input.c
@@ -3,6 +3,6 @@
 
 void main(int argc, char *argv[]){
 	char path[256];
-	sprintf(path,"%s%s", "/bin/", argv[1]);
-	execl(path, argv[1],argv[2],NULL);
+	sprintf(path,"%s%s", "/bin/", argv[1]); //경로 저장하기
+	execl(path, argv[1],argv[2],NULL); //경로에 있는 기본 명령어 실행하기
 }
